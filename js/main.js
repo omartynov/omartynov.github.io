9ab0e02944dea159a658f47d8479db1522005ea0
omartynov <olleg.martynov@gmail.com>
2016-12-24 15:28:36 +0700

    qwe

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function initSVG(objSVG){
 /* */
 	var a = objSVG.getSVGDocument();
-	objSVG.contentDocument.childNodes[0].addEventListener('click', function(){asd});
+	a.addEventListener('click', function(){asd});
 };
 
 function asd(){
