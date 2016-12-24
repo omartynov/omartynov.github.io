bcd10bf5080c28f5629b7d8b707cda3d30edec19
omartynov <olleg.martynov@gmail.com>
2016-12-24 18:59:18 +0700

    qwe

--- js/svg.js
+++ js/svg.js
@@ -6,6 +6,6 @@ function initSVG(objSVG){
 function loadSVG(){
 /* */
 	var r = new XMLHttpRequest();
-	r.open('get', '/images/', true);
+	r.open('get', 'php.php', true);
 	r.send();
 };
---
+++ php/php.php
@@ -0,0 +1,19 @@
+<?php 
+
+$dir = "./"; //default to root directory
+if (strlen($_GET['images']) > 0){$dir = $_GET['images'];}
+getList($dir);
+
+function getList($name) {
+    $path = realpath($name);
+    $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::CATCH_GET_CHILD);
+    foreach($objects as $name => $object){
+        if (filetype($name) == "dir") {
+            print "<br/><a href='javascript:void' onClick='do_it(\"" . $path . "/" . basename($name) . "\")'><b>" . basename($name) . "</b></a>";
+        } else {
+            print "<br/><a><b>" . basename($name) . "</b></a>";
+        }
+    }
+}
+
+?>
