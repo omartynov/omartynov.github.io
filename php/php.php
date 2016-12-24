086eadbcf5f0beea5ef357e9ec71f767fc313535
omartynov <olleg.martynov@gmail.com>
2016-12-24 19:10:16 +0700

    awe

--- php/php.php
+++ php/php.php
@@ -1,19 +1,22 @@
-<?php 
+<?php
 
-$dir = "./"; //default to root directory
-if (strlen($_GET['images']) > 0){$dir = $_GET['images'];}
-getList($dir);
+$dir          = "images/";
+$return_array = array();
 
-function getList($name) {
-    $path = addslashes(realpath($name));
-    $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::CATCH_GET_CHILD);
-    foreach($objects as $name => $object){
-        if (filetype($name) == "dir") {
-            print "<br/><a href='javascript:void' onClick='do_it(\"" . $path . "/" . basename($name) . "\")'><b>" . basename($name) . "</b></a>";
-        } else {
-            print "<br/><a><b>" . basename($name) . "</b></a>";
+if(is_dir($dir)){
+
+    if($dh = opendir($dir)){
+        while(($file = readdir($dh)) != false){
+
+            if($file == "." or $file == ".."){
+
+            } else {
+                $return_array[] = $file; // Add the file to the array
+            }
         }
     }
+
+    echo json_encode($return_array);
 }
 
 ?>
