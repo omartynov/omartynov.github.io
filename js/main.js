592960df8ee95ca74679b2573006130c39beadff
omartynov <olleg.martynov@gmail.com>
2016-12-24 16:13:49 +0700

    asd

--- js/main.js
+++ js/main.js
@@ -2,7 +2,19 @@ function initSVG(objSVG){
 /* */
 	var a = objSVG.getSVGDocument();
 	var b = a.getElementById('qwe');
-	b.addEventListener('onClick', function(){asd});
+	b.addEventListener('click', function(){asd});
+	b.addEventListener('onclick', function(){asd});
+	b.addEventListener('click', function(){asd()});
+	b.addEventListener('onclick', function(){asd()});
+	b.addEventListener('click', asd);
+	b.addEventListener('onclick', asd);
+
+	objSVG.addEventListener('click', function(){asd});
+	objSVG.addEventListener('onclick', function(){asd});
+	objSVG.addEventListener('click', function(){asd()});
+	objSVG.addEventListener('onclick', function(){asd()});
+	objSVG.addEventListener('click', asd);
+	objSVG.addEventListener('onclick', asd);
 };
 
 function asd(){
