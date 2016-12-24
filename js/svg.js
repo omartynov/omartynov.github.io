827c7c0eb5e539838629f592c14c04bbe37b5b4b
omartynov <olleg.martynov@gmail.com>
2016-12-24 19:49:21 +0700

    qwe

--- js/svg.js
+++ js/svg.js
@@ -5,7 +5,26 @@ function initSVG(objSVG){
 
 function loadSVG(){
 /* */
-	var r = new XMLHttpRequest();
-	r.open('get', '/php/php.php', true);
-	r.send();
+	window.requestFileSystem(window.TEMPORARY, 1024*1024, onInitFs, errorHandler);
 };
+
+function onInitFs(fs) {
+
+  var dirReader = fs.root.createReader();
+  var entries = [];
+
+  // Call the reader.readEntries() until no more results are returned.
+  var readEntries = function() {
+     dirReader.readEntries (function(results) {
+      if (!results.length) {
+        listResults(entries.sort());
+      } else {
+        entries = entries.concat(toArray(results));
+        readEntries();
+      }
+    }, errorHandler);
+  };
+
+  readEntries(); // Start reading dirs.
+
+}
