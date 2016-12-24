c8a4b52bf3c3763d03ce5ba2940ef7ef6b1a5ffa
omartynov <olleg.martynov@gmail.com>
2016-12-24 16:22:38 +0700

    qwe

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@ function initSVG(objSVG){
 //	b.addEventListener('onclick', function(){asd});
 //	b.addEventListener('click', function(){asd()});
 //	b.addEventListener('onclick', function(){asd()});
-//	b.addEventListener('click', asd);
+	b.addEventListener('click', asd);
 //	b.addEventListener('onclick', asd);
 
 //	objSVG.addEventListener('click', function(){asd});
 //	objSVG.addEventListener('onclick', function(){asd});
-	objSVG.addEventListener('click', function(){asd()});
+//~	objSVG.addEventListener('click', function(){asd()});
 //	objSVG.addEventListener('onclick', function(){asd()});
 //~	objSVG.addEventListener('click', asd);
 //~	objSVG.addEventListener('onclick', asd);
