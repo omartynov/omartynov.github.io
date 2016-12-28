c781cfb40f74ecd6769dea233cefc7f22072b54e
omartynov <olleg.martynov@gmail.com>
2016-12-28 14:44:24 +0700

    qwe

--- js/svg.js
+++ js/svg.js
@@ -1,22 +1,14 @@
-function initSVG(objSVG){
-/* */
-	var a = objSVG.getSVGDocument();
-};
-
 function loadSVG(){
 	document.querySelectorAll('.container_svg').forEach(function(div){
 		var obj = document.createElement('object');
 		obj.data = '/images/garland/arch_yellow_red.svg';
 		obj.type = 'image/svg+xml';
-//		obj.click = function(){initSVG()};
-		obj.addEventListener('load', initSVG_1);
-		obj.addEventListener('click', function(){initSVG_1});
-//		obj.addEventListener('click', initSVG());
+		obj.addEventListener('load', initSVG(this));
 		div.appendChild(obj);
 	});
 };
 
-function initSVG_1(objSVG){
+function initSVG(objSVG){
 /* */
 	var a = objSVG.getSVGDocument();
 };
