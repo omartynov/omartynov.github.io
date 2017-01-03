056364e35068d159e283ed6872e20df32787aefd
omartynov <olleg.martynov@gmail.com>
2017-01-03 23:24:22 +0700

    aaa

--- js/svg.js
+++ js/svg.js
@@ -1,17 +1,17 @@
-function initSVG(objSVG){
+function initSVG(id){
 /* */
-	var a = objSVG.getSVGDocument();
+	var a = document.getElementById(id).getSVGDocument();
 };
 
 function loadSVG(){
 /* */
 	document.querySelectorAll('.svg_source').forEach(function(div){
 		var obj = document.createElement('object');
-		obj.data = '/images/garland/arch_yellow_red.svg';
+		obj.data = '/images/' + div.id + '/arch_yellow_red.svg';
+		obj.id = 'svg_source_arch_yellow_red';
 		obj.type = 'image/svg+xml';
-//		obj.onload = initSVG(obj);
-//		obj.addEventListener('SVGLoad', initSVG(obj));
 		div.appendChild(obj);
-		obj.onload = initSVG(obj);
+		
+		obj.addEventListener('load', initSVG(obj.id));
 	});
 };
