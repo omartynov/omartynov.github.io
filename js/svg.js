8849b0d95fc18473f69be1d2e0276a52531968a6
omartynov <olleg.martynov@gmail.com>
2017-01-03 21:57:45 +0700

    qwe

--- js/svg.js
+++ js/svg.js
@@ -1,14 +1,15 @@
-function loadSVG(){
-	document.querySelectorAll('.container_svg').forEach(function(div){
-		var obj = document.createElement('object');
-		obj.data = '/images/garland/arch_yellow_red.svg';
-		obj.type = 'image/svg+xml';
-		obj.addEventListener('load', initSVG(obj));
-		div.appendChild(obj);
-	});
-};
-
 function initSVG(objSVG){
 /* */
 	var a = objSVG.getSVGDocument();
+};
+
+function loadSVG(){
+/* */
+	document.querySelectorAll('.svg_source').forEach(function(div){
+		var obj = document.createElement('object');
+		obj.data = '/images/garland/arch_yellow_red.svg';
+		obj.type = 'image/svg+xml';
+		obj.addEventListener('SVGLoad', initSVG(obj));
+		div.appendChild(obj);
+	});
 };
