0b57f378367775c4765a17899c57cb5c3f5a0f8c
omartynov <olleg.martynov@gmail.com>
2017-01-07 19:58:43 +0700

    ccc

--- js/svg.js
+++ js/svg.js
@@ -1,7 +1,7 @@
 function initSVG(obj){
 /* */
 	var svg = obj.getSVGDocument().getElementById(obj.id.replace('source_', ''));
-	svg.addEventListener('mousedown', function(evt){selectSVG(evt);});
+	svg.addEventListener('mousedown', function(){selectSVG(evt);});
 };
 
 function loadSVG(){
