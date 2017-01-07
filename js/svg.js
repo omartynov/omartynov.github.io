ddd94f58cce4d61f85c7afe04987501a54463944
omartynov <olleg.martynov@gmail.com>
2017-01-07 22:02:20 +0700

    qwe

--- js/svg.js
+++ js/svg.js
@@ -1,6 +1,9 @@
 function initSVG(obj){
 /* */
 	var svg = obj.getSVGDocument().getElementById(obj.id.replace('source_', ''));
+	var b = obj.getSVGDocument()
+	b.draggable=true;
+	b.addEventListener('dragstart', function(event){console.log('aaa')});
 //	svg.addEventListener('mousedown', function(event){selectSVG(event);});
 //svg.draggable=true;
 svg.addEventListener('dragstart', function(event){console.log('aaa')});
