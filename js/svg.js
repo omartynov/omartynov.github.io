fed7c5dcea080b18eb8270271c12d7a8cbea7a89
omartynov <olleg.martynov@gmail.com>
2017-01-08 17:16:08 +0700

    qqq

--- js/svg.js
+++ js/svg.js
@@ -1,8 +1,8 @@
 function initSVG(obj){
 /* */
-	var svg = obj.getSVGDocument().getElementById(obj.id.replace('source_', ''));
-	var b = obj.getSVGDocument()
-	svg.draggable="yes";
+//	var svg = obj.getSVGDocument().getElementById(obj.id.replace('source_', ''));
+//	var b = obj.getSVGDocument()
+//	svg.draggable="yes";
 //	b.addEventListener('dragstart', function(event){console.log('aaa')});
 //	svg.addEventListener('mousedown', function(event){selectSVG(event);});
 //svg.draggable=true;
@@ -12,12 +12,13 @@ function initSVG(obj){
 function loadSVG(){
 /* */
 	document.querySelectorAll('.svg_source').forEach(function(div){
+	div.addEventListener('dragstart', function(event){alert('aaa')});
 		var obj = document.createElement('object');
 		obj.data = '/images/' + div.id + '/arch_yellow_red.svg';
 		obj.id = 'svg_source_arch_yellow_red';
 		obj.type = 'image/svg+xml';
-		obj.draggable="yes";
-		obj.addEventListener('dragstart', function(event){alert('aaa')});
+//		obj.draggable="yes";
+//		obj.addEventListener('dragstart', function(event){alert('aaa')});
 		obj.addEventListener('load', function(){initSVG(this);});
 		div.appendChild(obj);
 	});
